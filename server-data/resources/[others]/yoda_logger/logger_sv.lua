7f90cb7696a40d6f83be11dd871478c7799f5233
Drmarsupial35 <drmarsupial35@gmail.com>
2020-03-22 14:49:14 +0100

    :sparkles: Logs sur Discord

---
+++ server-data/resources/[others]/yoda_logger/logger_sv.lua
@@ -0,0 +1,43 @@
+local logs = "https://discordapp.com/api/webhooks/691280664548606032/9UIM3utag1V4jHxL34ED1mLs6W5y-2G5GWyd89W2zx6meode-0Nr229zwUJAGoUsfoho"
+local communityname = "Big Yoda"
+local communtiylogo = "https://storage.needpix.com/rsynced_images/camera-2412643_1280.png" --Must end with .png
+
+AddEventHandler('playerConnecting', function()
+local name = GetPlayerName(source)
+local ip = GetPlayerEndpoint(source)
+local ping = GetPlayerPing(source)
+local steamhex = GetPlayerIdentifier(source)
+local connect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Player Connected to Server #1",
+            ["description"] = "Player: **"..name.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+	        ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Big Yoda Server Logger", embeds = connect}), { ['Content-Type'] = 'application/json' })
+end)
+
+AddEventHandler('playerDropped', function(reason)
+local name = GetPlayerName(source)
+local ip = GetPlayerEndpoint(source)
+local ping = GetPlayerPing(source)
+local steamhex = GetPlayerIdentifier(source)
+local disconnect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Player Disconnected from Server #1",
+            ["description"] = "Player: **"..name.."** \nReason: **"..reason.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+	        ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Big Yoda Server Logger", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
+end)
