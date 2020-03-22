f0a9671f081038582f7846611a6e56e173f56c20
Drmarsupial35 <drmarsupial35@gmail.com>
2020-03-22 17:17:28 +0100

    Changement du plugin de logs

---
+++ server-data/resources/[others]/esx_discord_bot/config.lua
@@ -0,0 +1,39 @@
+Config                        = {}
+Config.Locale 				  = 'fr'
+Config.green 				  = 56108
+Config.grey 				  = 8421504
+Config.red 					  = 16711680
+Config.orange 				  = 16744192
+Config.blue 				  = 2061822
+Config.purple 				  = 11750815
+Config.webhook                = "https://discordapp.com/api/webhooks/691280664548606032/9UIM3utag1V4jHxL34ED1mLs6W5y-2G5GWyd89W2zx6meode-0Nr229zwUJAGoUsfoho"
+
+
+settings = {
+	LogKills = true, -- Log when a player kill an other player.
+	LogEnterPoliceVehicle = false, -- Log when an player enter in a police vehicle.
+	LogEnterBlackListedVehicle = true, -- Log when a player enter in a blacklisted vehicle.
+	LogPedJacking = true, -- Log when a player is jacking a car
+	LogChatServer = true, -- Log when a player is talking in the chat , /command works too.
+	LogLoginServer = true, -- Log when a player is connecting/disconnecting to the server.
+	LogItemTransfer = true, -- Log when a player is giving an item.
+	LogWeaponTransfer = true, -- Log when a player is giving a weapon.
+	LogMoneyTransfer = true, -- Log when a player is giving money
+	LogMoneyBankTransfert = true, -- Log when a player is giving money from bankaccount
+
+}
+
+
+
+blacklistedModels = {
+	"APC",
+	"BARRACKS",
+	"BARRACKS2",
+	"RHINO",
+	"CRUSADER",
+	"CARGOBOB",
+	"SAVAGE",
+	"TITAN",
+	"LAZER",
+	"LAZER",
+}
---
+++ server-data/resources/[others]/esx_discord_bot/locales/fr.lua
@@ -0,0 +1,33 @@
+Locales['fr'] = {
+	['server'] = 'Serveur',
+	['server_start'] = 'Le serveur a démarré',
+	['server_chat'] = 'chat',
+
+	['server_connecting'] = 'Nouvelle connexion',
+	['user_connecting'] = 's\'est connecté',
+
+	['server_disconnecting'] = 'Nouvelle déconnexion',
+	['user_disconnecting'] = 's\'est déconnecté',
+
+	['server_item_transfer'] = 'Nouvelle transaction (objet)',
+	['server_money_transfer'] = 'Nouvelle transaction (argent)',
+	['server_moneybank_transfer'] = 'Nouvelle transaction (argent banque)',
+	['server_weapon_transfer'] = 'Nouvelle transaction (arme)',
+
+	['user_gives_to'] = 'a donné a',
+
+	['server_washingmoney'] = 'Nouveau blanchiement';
+	['user_washingmoney'] = 'a blanchi',
+
+	['server_blacklistedvehicle'] = 'Véhicule interdit alerte',
+	['user_entered_in'] = 'est entré dans :',
+
+	['server_policecar'] = 'Vehicule police alerte',
+	['server_carjacking'] = 'Vehicule volé alerte',
+	['user_carjacking'] = 'a volé',
+
+	['server_kill'] = 'Nouvelle mort',
+	['user_kill'] = 'a été tué par',
+	['user_kill_environnement'] = 'est mort par suicide ou attaque PNJ',
+	['with'] ='avec',
+}
