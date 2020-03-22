2d9ed10daa88b4919c709ae1734edeae0e52a9b0
Drmarsupial35 <drmarsupial35@gmail.com>
2020-03-22 16:35:22 +0100

    :ok_hand: MAJ des logs

--- server-data/resources/[others]/yoda_logger/logger_sv.lua
+++ server-data/resources/[others]/yoda_logger/logger_sv.lua
@@ -1,5 +1,5 @@
 local logs = "https://discordapp.com/api/webhooks/691280664548606032/9UIM3utag1V4jHxL34ED1mLs6W5y-2G5GWyd89W2zx6meode-0Nr229zwUJAGoUsfoho"
-local communityname = "Big Yoda"
+local communityname = "Logs Bot"
 local communtiylogo = "https://storage.needpix.com/rsynced_images/camera-2412643_1280.png" --Must end with .png
 
 AddEventHandler('playerConnecting', function()
@@ -10,8 +10,8 @@ local steamhex = GetPlayerIdentifier(source)
 local connect = {
         {
             ["color"] = "8663711",
-            ["title"] = "Player Connected to Server #1",
-            ["description"] = "Player: **"..name.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+            ["title"] = "Un joueur s'est connecté au serveur",
+            ["description"] = "Joueur: **"..name.."**\nIP: **"..ip.."**\n Steam ID: **"..steamhex.."**",
 	        ["footer"] = {
                 ["text"] = communityname,
                 ["icon_url"] = communtiylogo,
@@ -19,7 +19,7 @@ local connect = {
         }
     }
 
-PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Big Yoda Server Logger", embeds = connect}), { ['Content-Type'] = 'application/json' })
+PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "CCTV Logs Bot", embeds = connect}), { ['Content-Type'] = 'application/json' })
 end)
 
 AddEventHandler('playerDropped', function(reason)
@@ -30,8 +30,8 @@ local steamhex = GetPlayerIdentifier(source)
 local disconnect = {
         {
             ["color"] = "8663711",
-            ["title"] = "Player Disconnected from Server #1",
-            ["description"] = "Player: **"..name.."** \nReason: **"..reason.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+            ["title"] = "Un joueur s'est déconnecté au serveur",
+            ["description"] = "Joueur: **"..name.."** \nRaison: **"..reason.."**\nIP: **"..ip.."**\n Steam ID: **"..steamhex.."**",
 	        ["footer"] = {
                 ["text"] = communityname,
                 ["icon_url"] = communtiylogo,
@@ -39,5 +39,5 @@ local disconnect = {
         }
     }
 
-    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Big Yoda Server Logger", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "CCTV Logs Bot", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
 end)
