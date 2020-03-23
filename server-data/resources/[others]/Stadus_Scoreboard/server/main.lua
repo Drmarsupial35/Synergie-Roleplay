2a85b8548dd2e11fadb1d7a7ca0fa432d22b4026
Drmarsupial35 <drmarsupial35@gmail.com>
2020-03-23 20:12:05 +0100

    Changement script pour le scoreboard

--- server-data/resources/[others]/Stadus_Scoreboard/server/main.lua
+++ server-data/resources/[others]/Stadus_Scoreboard/server/main.lua
@@ -45,14 +45,14 @@ function AddPlayerToScoreboard(xPlayer, update)
 	connectedPlayers[playerId] = {}
 	connectedPlayers[playerId].ping = GetPlayerPing(playerId)
 	connectedPlayers[playerId].id = playerId
-	connectedPlayers[playerId].name = Sanitize(xPlayer.getName())
+	connectedPlayers[playerId].name = xPlayer.getName()
 	connectedPlayers[playerId].job = xPlayer.job.name
 
 	if update then
 		TriggerClientEvent('esx_scoreboard:updateConnectedPlayers', -1, connectedPlayers)
 	end
 
-	if xPlayer.getGroup() == 'user' then
+	if xPlayer.player.getGroup() == 'user' then
 		Citizen.CreateThread(function()
 			Citizen.Wait(3000)
 			TriggerClientEvent('esx_scoreboard:toggleID', playerId, false)
@@ -79,25 +79,14 @@ function UpdatePing()
 	TriggerClientEvent('esx_scoreboard:updatePing', -1, connectedPlayers)
 end
 
-function Sanitize(str)
-	local replacements = {
-		['&' ] = '&amp;',
-		['<' ] = '&lt;',
-		['>' ] = '&gt;',
-		['\n'] = '<br/>'
-	}
-
-	return str
-		:gsub('[&<>\n]', replacements)
-		:gsub(' +', function(s)
-			return ' '..('&nbsp;'):rep(#s-1)
-		end)
-end
-
-ESX.RegisterCommand('screfresh', 'admin', function(xPlayer, args, showError)
+TriggerEvent('es:addGroupCommand', 'screfresh', 'admin', function(source, args, user)
 	AddPlayersToScoreboard()
-end, false, {help = 'Refresh esx_scoreboard names!'})
+end, function(source, args, user)
+	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
+end, {help = "Refresh esx_scoreboard names!"})
 
-ESX.RegisterCommand('sctoggle', 'admin', function(xPlayer, args, showError)
-	xPlayer.triggerEvent('esx_scoreboard:toggleID')
-end, false, {help = 'Toggle ID column on the scoreboard!'})
+TriggerEvent('es:addGroupCommand', 'sctoggle', 'admin', function(source, args, user)
+	TriggerClientEvent('esx_scoreboard:toggleID', source)
+end, function(source, args, user)
+	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
+end, {help = "Toggle ID column on the scoreboard!"})
