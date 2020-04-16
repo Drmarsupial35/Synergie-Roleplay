155dcca51d4db59be78927a254c305a9fdbf6db2
Drmarsupial35 <drmarsupial35@gmail.com>
2020-04-16 19:25:43 +0200

    Changement du script pour cacher le viseur

--- server-data/resources/[others]/randomScripts/__resource.lua
+++ server-data/resources/[others]/randomScripts/__resource.lua
@@ -2,7 +2,6 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 
 client_scripts {
-	'client/disableCrosshair.lua',
 	'client/disableWeapons.lua',
 	'client/friendlyNPC.lua',
 	'client/lessNPC.lua',
