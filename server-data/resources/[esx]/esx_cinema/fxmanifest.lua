a32a141f2402251d24a53adcdacd0774abfe7f58
Drmarsupial35 <drmarsupial35@gmail.com>
2020-04-27 23:53:15 +0200

    :sparkles: Ajout de cinémas

---
+++ server-data/resources/[esx]/esx_cinema/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Cinema'
+
+version '1.2.0'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'config.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'locales/sv.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'config.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'locales/sv.lua',
+	'client/main.lua'
+}
