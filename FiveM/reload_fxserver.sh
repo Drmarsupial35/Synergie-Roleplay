0ac97550609d6fc23172b047e0078e99b85f22b3
Drmarsupial35 <drmarsupial35@gmail.com>
2020-05-01 23:56:35 +0200

    Configuration du restart sur le serveur

--- FiveM/reload_fxserver.sh
+++ FiveM/reload_fxserver.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "***********************************************************"
-echo `date '+%d-%B-%Y_%H:%M:%S'` " - Arrêt du serveur..."
-kill -9 `ps -ef | grep "/home/fivem/Synergie-Roleplay/FiveM/proot" | grep -v grep | awk '{print $2}'`
+echo `date '+%d-%B-%Y_%H:%M:%S'`" - Arrêt du serveur..."
+kill -9 `ps -ef | grep "/home/fivem/Synergie-Roleplay/FiveM" | grep -v grep | awk '{print $2}'`
 sleep 2
 echo `date '+%d-%B-%Y_%H:%M:%S'`" - Redémarrage de mysql..."
 sudo service mysql restart
 sleep 10
-echo `date '+%d-%B-%Y_%H:%M:%S'` " - Nettoyage du cache..."
+echo `date '+%d-%B-%Y_%H:%M:%S'`" - Nettoyage du cache..."
 rm -R /home/fivem/Synergie-Roleplay/FiveM/server-data/cache/
 sleep 2
-echo `date '+%d-%B-%Y_%H:%M:%S'` " - Démarrage du serveur..."
+echo `date '+%d-%B-%Y_%H:%M:%S'`" - Démarrage du serveur..."
 screen -x Live-Server -X stuff 'cd /home/fivem/Synergie-Roleplay/FiveM/server-data/
-/home/fivem/Synergie-Roleplay/FiveM/run.sh +exec server.cfg
+../run.sh +exec server.cfg
 '
 sleep 10
-echo `date '+%d-%B-%Y_%H:%M:%S'` " - Fin de la procédure"
+echo `date '+%d-%B-%Y_%H:%M:%S'`" - Fin de la procédure"
