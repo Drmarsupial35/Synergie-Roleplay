0d8a6433b1f110f75fef8e1516f3a6e12aecc100
Drmarsupial35 <drmarsupial35@gmail.com>
2020-05-03 21:39:25 +0200

    Désactivation des coups de crosse

--- FiveM/server-data/resources/[others]/randomScripts/__resource.lua
+++ FiveM/server-data/resources/[others]/randomScripts/__resource.lua
@@ -2,6 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 
 client_scripts {
+	'client/disableReloadKill.lua',
 	'client/disableWeapons.lua',
 	'client/friendlyNPC.lua',
 	'client/lessNPC.lua',
