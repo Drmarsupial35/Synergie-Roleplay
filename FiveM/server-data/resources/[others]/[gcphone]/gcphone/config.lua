92707c6f79912a483e08a7d11268a499ec376cfb
Drmarsupial35 <drmarsupial35@gmail.com>
2020-05-13 22:41:45 +0200

    :sparkles: Ajout du téléphone + Carte SIM

---
+++ FiveM/server-data/resources/[others]/[gcphone]/gcphone/config.lua
@@ -0,0 +1,6 @@
+FixePhone = {
+  -- Poste de police
+  ['911'] = { name =  "Central Police", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+}
+
+ShowNumberNotification = true -- Show Number or Contact Name when you receive new SMS
