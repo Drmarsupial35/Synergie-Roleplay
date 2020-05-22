8d89978682b468282c6831873ebb034646c541c6
Drmarsupial35 <drmarsupial35@gmail.com>
2020-05-22 17:28:11 +0200

    :sparkles: Ajout du braquage de la bijouterie

---
+++ FiveM/server-data/resources/[esx]/esx_vangelico_robbery/locales/fr.lua
@@ -0,0 +1,38 @@
+Locales['fr'] = {
+
+	['robbery_cancelled'] = 'Le braquage a été annulé',
+	['shop_robbery'] = 'Bijouterie',
+	['press_to_rob'] = 'tirer pour commencer le braquage',
+	['seconds_remaining'] = '~w~ secondes restantes',
+	['robbery_cancelled_at'] = '~r~Le braquage a été annulé à la ~b~',
+	['robbery_has_cancelled'] = '~r~Le braquage a été annulé: ~b~',
+	['already_robbed'] = 'les bijoux ont déjà été volés. Veuillez attendre: ',
+	['seconds'] = ' secondes.',
+	['rob_in_prog'] = '~r~Braquage en cours à la ~b~',
+	['started_to_rob'] = 'Vous commencer le braquage ',
+	['do_not_move'] = ', prenez les bijoux dans les vitrines !',
+	['alarm_triggered'] = 'l\'alarme a été déclenchée',
+	['hold_pos'] = 'Quand vous aurez récupéré le maximum de bijoux, fuyez !',
+	['robbery_complete'] = '~r~ Le braquage a reussi !~s~ ~h~ enfuyez vous ! ',
+	['robbery_complete_at'] = '~r~ Le braquage a réussi à: ~b~',
+	['min_two_police'] = 'Il doit y avoir au moins ~b~',
+	['min_two_police2'] = ' ~w~policiers en ville.',
+	['robbery_already'] = '~r~Un braquage est déjà en cours.',
+	['robbery_has_ended'] = 'Braquage terminé',
+	['end'] = 'La bijouterie a été braquée!',
+	['notenoughgold'] = '~r~Vous n\'avez pas assez de bijoux !',
+	['copsforsell'] = 'Il doit y avoir au moins ~b~',
+	['copsforsell2'] = ' ~w~policiers en ville pour revendre les bijoux.',
+	['goldsell'] = '~y~Bijoux~s~ vente en cours',
+	['field'] = 'Pressez ~y~E~s~ pour ~o~voler~s~ les bijoux',
+	['collectinprogress'] = 'récupération des bijoux en cours...',
+	--['lester'] = '~g~Vous avez braqué la bijouterie! Maintenant, revendez les bijoux !', --if lester map blips hide
+	['lester'] = '~g~Vous avez braqué la bijouterie! Revendez les bijoux que vous avez attrapé à ~r~Lester!',-- if Lester map blip enable
+	['lester2'] = '~g~Vous avez quitté la bijouterie ! Revendez les bijoux que vous avez attrapé à ~r~Lester!',-- if Lester map blip enable
+	['jewelsblipmap'] = 'Bijouterie',
+	['press_to_collect'] = 'pour voler les bijoux',
+	['smash_case'] = 'Vitrines brisées',
+	['press_to_sell'] = 'Pressez ~INPUT_PICKUP~ pour vendre les bijoux',
+	['need_bag'] = 'vous avez besoin d\'un sac ! allez dans un magasin de vêtements '
+
+}
