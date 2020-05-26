bef40bcd689def11c700e2db760653e84c14c8e4
Drmarsupial35 <drmarsupial35@gmail.com>
2020-05-26 22:47:36 +0200

    :sparkles: Ajout du blanchisseur

---
+++ FiveM/server-data/resources/[esx]/esx_blanchisseur/config.lua
@@ -0,0 +1,15 @@
+Config                          = {}
+Config.Locale                   = 'fr'
+Config.Slice                    = 10000
+Config.Percentage               = 50
+Config.MarkerSize               = {x = 3.0, y = 3.0, z = 3.0}
+Config.openHours                = 0
+Config.closeHours               = 5
+Config.Blip                     = false
+Config.Hours                    = false
+Config.Menu                     = false
+Config.Bonus                    = {min = 0, max = 05}
+
+Config.Zones = {
+	{x = 1131.328, y = -3197.172, z = -40.666,percent=30},
+}
