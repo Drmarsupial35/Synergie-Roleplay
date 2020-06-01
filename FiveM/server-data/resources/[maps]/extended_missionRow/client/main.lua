a6599f9a18cae56bcae9dd195194ac9eef5abea8
Drmarsupial35 <drmarsupial35@gmail.com>
2020-06-01 18:23:06 +0200

    :bug: Les PNJs ne spawnent plus dans le comico

--- FiveM/server-data/resources/[maps]/extended_missionRow/client/main.lua
+++ FiveM/server-data/resources/[maps]/extended_missionRow/client/main.lua
@@ -4,7 +4,7 @@ end)
 
 Citizen.CreateThread(function()
     while (true) do
-        ClearAreaOfPeds(440.84, -983.14, 30.69, 300, 1)
+        ClearAreaOfPeds(456.729, -985.872, 30.689, 10000, 1)
         Citizen.Wait(0)
     end
 end)
